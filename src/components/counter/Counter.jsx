03d80785ff7312c316cbbd8ad2f6fe2786aac091
Priyanshi Agarwal <priyanshiagarwal@Priyanshis-MacBook-Pro.local>
2023-10-23 00:00:55 +0530

    Add state to counter using 'useState' Hook

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,27 +1,38 @@
-export default function Counter() {
-    
-    const buttonStyle = {
-        fontSize:"16px",
-        backgroundColor: "#00a5ab",
-        width:"100px",
-        margin:"10px",
-        color:"white",
-        padding:"15px",
-        borderRadius:"30px"
-    };
+import { useState } from 'react'
+import './Counter.css'
 
-    function incrementCounterFunction() {
+export default function Counter() {
+
+    // useState(0) ==[0,f] (initial value, function)
+    // const state = useState(0);
+    // state[0] is value of state
+    // state[1] is a function
+    const [count,setCount] = useState(0);
+
+    function incrementCounterFunction() { 
+        // state[1](state[0]+1) // this is increasing the value of state by 1 on every click
+        setCount(count+1)
+        console.log(count)
         console.log('increment cliclked')
     }
-
+    // we are updating the state and view is automatically updated
+    function decrementCounterFunction() { 
+        setCount(count-1)
+        console.log(count)
+        console.log('decrement cliclked')
+    }
 
     return (
         <div className="Counter">
-            <span className="count">0</span>
-            <button className="counterButton" 
-                    onClick={incrementCounterFunction}
-                    style = {buttonStyle}
-            >+1</button>
+            <span className="count">{count}</span>
+            <div>
+                <button className="counterButton" 
+                        onClick={incrementCounterFunction}
+                >+1</button>
+                <button className="counterButton" 
+                        onClick={decrementCounterFunction}
+                >-1</button>
+            </div>
         </div>
     )
 }
