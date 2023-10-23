0928fd9108cfa7171cf69798604e6885d1cc63ac
Priyanshi Agarwal <priyanshiagarwal@Priyanshis-MacBook-Pro.local>
2023-10-24 00:44:15 +0530

    basic change

--- src/components/counter/CounterButton.jsx
+++ src/components/counter/CounterButton.jsx
@@ -4,12 +4,12 @@ export default function CounterButton({by, incrementMethod, decrementMethod}) {
     return (
         <div className="Counter">
             <button className="counterButton" 
-                    onClick={() => incrementMethod(by)}
-            >+{by}</button>
-
-            <button className="counterButton" 
                     onClick={() => decrementMethod(by)}
             >-{by}</button>
+
+            <button className="counterButton" 
+                    onClick={() => incrementMethod(by)}
+            >+{by}</button>
         </div>
     )
 }
